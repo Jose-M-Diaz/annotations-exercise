0de739b68832ca360ff3d214ab4eaf8a5f082584
Jose Diaz <Diaz.M.Jose68@gmail.com>
2022-01-13 10:06:12 -0600

    completed better-fix walkthrough

--- src/Author.java
+++ src/Author.java
@@ -4,13 +4,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("all")
+
 public class Author extends Person {
-    private List books;
+    private final List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
